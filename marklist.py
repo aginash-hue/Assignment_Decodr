411fd4ce400c8c447db084ed959b50b6f2a02a56
Aginash Mannarath <64060826+aginash-hue@users.noreply.github.com>
2021-12-02 07:02:51 -0800

    Update marklist.py

--- marklist.py
+++ marklist.py
@@ -12,11 +12,11 @@ mark_in_science = int(input("Enter your Science mark out of 100:"))
 mark_in_malayalam = int(input("Enter your Malayalam mark out of 100:"))
 total = mark_in_maths + mark_in_science + mark_in_malayalam 
 if total > 90 and mark_in_maths >= 30 and mark_in_science >= 30 and mark_in_malayalam >=30 :
-    if total >= 270:
+    if total >= 270 :
         print("congratulation you passed with distinction")
     elif total >= 240 and total < 270:
         print("congratulation you passed with first class")
     elif total >= 90 and total < 240:
         print("congratulation you passed with second class")
 else :
-    print("you failed")
+    print("Sorry you failed")
